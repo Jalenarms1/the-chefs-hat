17abb7e182872cb94dd6a3cf634e88223e0c83cf
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-09-27 15:13:41 -0700

    added login and sign up routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,9 @@
 // Place script code for routes here
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const navRoutes = require("./directing-routes.js");
+
+router.use("/", navRoutes);
+router.use("/api", apiRoutes);
+
+module.exports = router;
