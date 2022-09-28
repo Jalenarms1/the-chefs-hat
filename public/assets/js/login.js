6e4555b1c10b58fcc22059d1e92fc7b1f9f72eaf
Jayita Chowdhury <jayitachowdhury@Amits-MacBook-Pro.local>
2022-09-27 19:20:49 -0700

    modifing login.js and added logout.js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -3,15 +3,15 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the login form 
-    const email = document.querySelector ('#emailLog').value.trim();
+    const email = document.querySelector('#emailLog').value.trim();
     const password = document.querySelector('#passLog').value.trim();
 
     if (email && password) {
         // Send a Post request to the API endpoint
-        const response = await fetch ('/api/users/login', {
+        const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
         });
         if (response.ok) {
             document.location.replace('/profile');
@@ -24,21 +24,26 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#formName').value.trim();
+    const owner = document.querySelector('#formName').value.trim();
     const email = document.querySelector('#formEmail').value.trim();
-  const password = document.querySelector('#formPass').value.trim();
+    const password = document.querySelector('#formPass').value.trim();
+    const address = document.querySelector('#address').value.trim();
+const phoneNumber = document.querySelector('#formPhone').value.trim();
+    const restName = document.querySelector('#formRest').value.trim();
 
-  if (name && email && password) {
-    const response = await fetch('/api/users/signup', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
+
+    if (owner && email && password && address && phoneNumber && restName) {
+        const response = await fetch('/api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({ ownername, email, password, address, phoneNumber, restName }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
     }
-  }
 };
